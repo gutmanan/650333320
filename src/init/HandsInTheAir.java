3533dce78acdf610902399702e40b8b6633f015e
Shai Gutman <Shai Gutman@ShaiGutman-PC>
2017-02-16 12:46:53 +0200

    Mainlogin was updated

--- src/init/HandsInTheAir.java
+++ src/init/HandsInTheAir.java
@@ -24,8 +24,8 @@ public class HandsInTheAir {
             //Define log file
             String dateNow = (new SimpleDateFormat("ddM_hhmm")).format(new Date());
             logFile = new PrintStream(new File(fileName+"_"+dateNow+".log"));
-            System.setErr(logFile);
-            System.setOut(logFile);
+            //System.setErr(logFile);
+            //System.setOut(logFile);
             
             DB = new DBManager();
             PDF = new PDFManager();
